3d3be2d17a959b1a69845e75bf14864732ef7c0c
Nam Do <namdo1204@gmail.com>
2024-03-11 10:38:32 -0700

    Added frontend app

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true, node: true },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+    ],
+    ignorePatterns: ["dist", ".eslintrc.cjs"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh"],
+    rules: {
+        "react-refresh/only-export-components": [
+            "warn",
+            { allowConstantExport: true },
+        ],
+        "linebreak-style": 0,
+        "object-curly-spacing": "off",
+        "new-cap": "error",
+        "no-unused-vars": "off",
+        "require-jsdoc": "error",
+        "@typescript-eslint/semi": ["error"],
+        "@typescript-eslint/require-await": "error",
+        "@typescript-eslint/explicit-function-return-type": "error",
+        "@typescript-eslint/explicit-module-boundary-types": "error",
+        "@typescript-eslint/restrict-template-expressions": "error",
+        "@typescript-eslint/restrict-plus-operands": "error",
+        "@typescript-eslint/no-unsafe-member-access": "error",
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            { argsIgnorePattern: "^_" },
+        ],
+        "no-case-declarations": "off",
+        indent: ["error", 4],
+        "max-len": [
+            "warn",
+            {
+                ignoreTemplateLiterals: true,
+                ignoreStrings: true,
+            },
+        ],
+    },
+};
