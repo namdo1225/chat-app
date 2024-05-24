0411e74db5f79b448da4567228ee2a7cb8274179
Nam Do <namdo1204@gmail.com>
2024-05-24 15:44:38 -0700

    Fixed ws import error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import { PORT } from "./utils/config";
 import { info } from "./utils/logger";
-import { server } from "./ws";
+import { server } from "./homeWS";
 
 server.listen(PORT, () => {
     info(`Server running on port ${PORT}`);
